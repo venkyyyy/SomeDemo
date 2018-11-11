274f7620e7455c43c6cc15b416ec83fcacc5b9f6
venkyyyy <venkyanthony@gmail.com>
2018-11-11 19:47:19 +0530

    Add files via upload

---
+++ books/spiders/books.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["www.google.co.in"]
+    start_urls = [
+        'https://www.google.co.in/search?q=Prime+Air+in+US-WA-Seattle&ibp=htl;jobs',
+    ]
+
+
+
+    def parse(self, response):
+          yield {'body':response.body}    
