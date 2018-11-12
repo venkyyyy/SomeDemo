8553d036e952bafbd3c5994ee4bfd4e01b3795c6
venkyyyy <venkyanthony@gmail.com>
2018-11-12 17:57:12 +0530

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -5,6 +5,12 @@ logger = logging.getLogger('mycustomlogger')
 
 class BooksSpider(scrapy.Spider):
     name = 'myspider'
-    logger.info(url)
+    #logger.info(url)
     logger.info('Hello World')
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.w3schools.com/', callback=self.parse)
+
+    def parse(self, response):
+        page = response.url
+        
     
