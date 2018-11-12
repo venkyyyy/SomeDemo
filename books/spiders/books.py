c47b39d382b5e4bfe71081ba2198896e07da3de2
venkyyyy <venkyanthony@gmail.com>
2018-11-12 10:04:22 +0530

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -10,7 +10,9 @@ class BooksSpider(scrapy.Spider):
     start_urls = [
         'https://www.google.co.in/search?q=Prime+Air+in+US-WA-Seattle&ibp=htl;jobs',
     ]
-
+     for url in start_urls:
+            # We make a request to each url and call the parse function on the http response.
+			yield scrapy.Request(url=url, callback=self.parse)
 
 
     def parse(self, response):
