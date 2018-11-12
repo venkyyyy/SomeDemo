7fce706d0817f5120905490732bd064994f0c2ba
venkyyyy <venkyanthony@gmail.com>
2018-11-12 17:51:50 +0530

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,17 +1,10 @@
 import logging
 import scrapy
-from ..items import BooksItem
-import os
 
 logger = logging.getLogger('mycustomlogger')
 
 class BooksSpider(scrapy.Spider):
-
     name = 'myspider'
-    #start_urls = ['https://scrapinghub.com']
-    url = os.environ['HTTP_HOST']
-
-    #def parse(self, response):
-    #url_get = self.request.GET
     logger.info(url)
     logger.info('Hello World')
+    
