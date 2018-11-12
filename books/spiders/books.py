1f1e1c9ae5b21a6a8fb64a7b5fcf950d0e23fd7c
venkyyyy <venkyanthony@gmail.com>
2018-11-12 10:23:45 +0530

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,22 +1,12 @@
-# -*- coding: utf-8 -*-
-import scrapy
-from ..items import BooksItem
 import logging
+import scrapy
 
+logger = logging.getLogger('mycustomlogger')
 
 class BooksSpider(scrapy.Spider):
-    name = "books"
-    allowed_domains = ["www.google.co.in"]
-    start_urls = [
-        'https://www.google.co.in/search?q=Prime+Air+in+US-WA-Seattle&ibp=htl;jobs',
-    ]
-     for url in start_urls:
-     	yield scrapy.Request(url=url, callback=self.parse)
 
+    name = 'myspider'
+    start_urls = ['https://scrapinghub.com']
 
     def parse(self, response):
-          item= BooksItem()
-          item['body']=response.body
-          #self.log(" Entered !! ")
-          logging.warning("This is a warning")
-          yield item
+        logger.info('Parse function called on %s', response.url)
