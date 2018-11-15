3b446217cc02cec32b65863c73ad08a407fd649a
venkyyyy <venkyanthony@gmail.com>
2018-11-15 18:46:44 +0530

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,12 +1,17 @@
 import scrapy
-class BooksSpider(scrapy.Spider):
-    name = 'myspider'
-    #self.log('Hello World')
-    def start_requests(self):
-        yield scrapy.Request(url='https://www.w3schools.com/', callback=self.parse)
 
-    def parse(self, response):
-        page = response.headers
-        self.log("Hello World")
-        self.log('My ip : '%page)        
-    
+
+class QuotesSpider(scrapy.Spider):
+   name = "quotes"
+   start_urls = [
+      'https://www.google.com/search?q=jobs+near+me&ibp=htl;jobs&sa=X&ved=2ahUKEwi56qSsu9beAhUEo48KHSKfDmYQiYsCKAB6BAgGEAM#fpstate=tldetail&htidocid=4UtjOLd3b4R-NFTwAAAAAA%3D%3D&htivrt=jobs'
+   ]
+
+   def parse(self, response):
+       #for quote in response.css('div.gRTukd T2uyV'):
+       #with open('D://outputs//hie.txt','w',encoding='utf-8') as U:
+       # U.write(str(response.body))
+       
+       yield{
+       'body':response.body
+       } 
