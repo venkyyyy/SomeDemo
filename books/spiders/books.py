52c66e5c3b3739c87aecfb576bf20f9883a9982f
venkyyyy <venkyanthony@gmail.com>
2018-11-15 19:03:18 +0530

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,17 +1,18 @@
 import scrapy
+from scrapy_splash import SplashRequest
 
+class MySpider(scrapy.Spider):
+    name="quotes"
+    start_urls = ["https://www.google.com/search?q=jobs+near+me&ibp=htl;jobs&sa=X&ved=2ahUKEwi56qSsu9beAhUEo48KHSKfDmYQiYsCKAB6BAgGEAM#fpstate=tldetail&htidocid=4UtjOLd3b4R-NFTwAAAAAA%3D%3D&htivrt=jobs"]
 
-class QuotesSpider(scrapy.Spider):
-   name = "quotes"
-   start_urls = [
-      'https://www.google.com/search?q=jobs+near+me&ibp=htl;jobs&sa=X&ved=2ahUKEwi56qSsu9beAhUEo48KHSKfDmYQiYsCKAB6BAgGEAM#fpstate=tldetail&htidocid=4UtjOLd3b4R-NFTwAAAAAA%3D%3D&htivrt=jobs'
-   ]
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse,
+                endpoint='render.html',
+                args={'wait': 0.5},
+            )
 
-   def parse(self, response):
-       #for quote in response.css('div.gRTukd T2uyV'):
-       #with open('D://outputs//hie.txt','w',encoding='utf-8') as U:
-       # U.write(str(response.body))
-       
-       yield{
-       'body':response.body
-       } 
+    def parse(self, response):
+        yeild{
+          'body': response.body
+        }
